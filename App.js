e569a0e60fe03015ef60812596e8ff30a8c92936
Megyeri Márk Máté <megyko1234@gmail.com>
2023-04-18 08:31:24 +0200

    Számítási objektumok megírása

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
+/*
+* File: App.js
+* Author: Megyeri Márk Máté
+* Copyright: 2023,  Megyeri Márk Máté
+* Group: Szoft II/N
+* Date: 2023-04-017
+* Github: https://github.com/megyerimark/
+* Licenc: GNU GPL
+*/
+
+
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const[long , setLong]=useState('');
+  const[meter, setPerimeter]=useState('');
+  const[result , setResult]=useState('');
+
+  function calc(){
+    let result = (meter*meter*long)/11877;
+    setResult(result);
+    
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ló súly számítás feladat 0375</Text>
       <StatusBar style="auto" />
     </View>
   );
