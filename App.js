20f5d3e9f08883780070805188b76d4a2aada30e
Megyeri Márk Máté <megyko1234@gmail.com>
2023-04-18 08:43:40 +0200

    A kész feladat

--- App.js
+++ App.js
@@ -12,22 +12,43 @@
 
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableHighlight, View } from 'react-native';
 
 export default function App() {
   const[long , setLong]=useState('');
-  const[meter, setPerimeter]=useState('');
+  const[meter, setMeterr]=useState('');
   const[result , setResult]=useState('');
 
   function calc(){
     let result = (meter*meter*long)/11877;
     setResult(result);
-    
+
   }
   return (
     <View style={styles.container}>
       <Text>Ló súly számítás feladat 0375</Text>
+
+
+
+      <Text >Mellkas kerülete(cm)</Text>
+          <TextInput  onChangeText={(res) => setMeterr(res)}></TextInput>
+
+          <Text >Hossz(cm)</Text>
+          <TextInput  onChangeText={(res) => setLong(res)}></TextInput>
+
+          <TouchableHighlight onPress={calc} >
+            <Text>Számít</Text>
+          </TouchableHighlight>
+
+
+          <Text>Eredmény</Text>
+          <TextInput value={result} onChangeText={(res)=> setResult(res)}></TextInput>
       <StatusBar style="auto" />
+
+
+
+<Text> Megyeri Márk Máté ,SZOFT II/N 2023.04.17</Text>
+
     </View>
   );
 }
